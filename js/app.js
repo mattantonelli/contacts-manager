8fdd19cb961d642a6a58f8e3ae34adb8502ad3f1
Matt Antonelli <mattr.antonelli@gmail.com>
2015-07-01 12:25:33 -0400

    Tutorial part 2
    * Added filter
      * The filter select allows you to filter your contacts by a category
      * Added a router to make different filters linkable

--- js/app.js
+++ js/app.js
@@ -45,12 +45,17 @@
 		initialize: function () {
 			this.collection = new Directory(contacts);
 			this.render();
+
+			this.$el.find("#filter").append(this.createSelect());
+			this.on("change:filterType", this.filterByType, this);
+			this.collection.on("reset", this.render, this);
 		},
 
 		render: function () {
-			var that = this;
+			this.$el.find("article").remove();
+
 			_.each(this.collection.models, function (item) {
-				that.renderContact(item);
+				this.renderContact(item);
 			}, this);
 		},
 
@@ -59,9 +64,72 @@
 				model: item
 			});
 			this.$el.append(contactView.render().el);
+		},
+
+		getTypes: function () {
+			return _.uniq(this.collection.pluck("type"), false, function (type) {
+				return type.toLowerCase();
+			});
+		},
+
+		createSelect: function () {
+			var select = $("<select/>", {
+					html: '<option value="all">all</option>'
+				});
+
+			_.each(this.getTypes(), function (item) {
+				var option = $("<option/>", {
+					value: item.toLowerCase(),
+					text: item.toLowerCase()
+				}).appendTo(select);
+			});
+
+			return select;
+		},
+
+		events: {
+			"change #filter select": "setFilter"
+		},
+
+		setFilter: function (e) {
+			this.filterType = e.currentTarget.value;
+			this.trigger("change:filterType");
+		},
+
+		filterByType: function () {
+			if (this.filterType === "all") {
+				this.collection.reset(contacts);
+				contactsRouter.navigate("filter/all");
+			} else {
+				this.collection.reset(contacts, { silent: true });
+
+				var filterType = this.filterType,
+					filtered = _.filter(this.collection.models, function (item) {
+						return item.get("type").toLowerCase() === filterType;
+					});
+
+				this.collection.reset(filtered);
+				contactsRouter.navigate("filter/" + filterType);
+			}
+		}
+	});
+
+	// routes
+	var ContactsRouter = Backbone.Router.extend({
+		routes: {
+			"filter/:type": "urlFilter"
+		},
+
+		urlFilter: function (type) {
+			directory.filterType = type;
+			directory.trigger("change:filterType");
 		}
 	});
 
 	// create instance of master view
 	var directory = new DirectoryView();
+
+	// create an instance of the contacts router
+	var contactsRouter = new ContactsRouter();
+	Backbone.history.start();
 } (jQuery));
